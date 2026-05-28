23fc4e5af0bfbb47abecedd7c58beb0f967cbc50
Zachary Tarvid-Richey <zrichey@pronetsinc.com>
2026-05-28 09:15:34 -0400

    Changes

--- init.lua
+++ init.lua
@@ -396,7 +396,9 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.opt.termguicolors = true
+  vim.o.background = 'light'
+  vim.cmd.colorscheme = 'quiet'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
@@ -699,12 +701,10 @@ do
     -- But for many setups, the LSP (`ts_ls`) will work just fine
     ts_ls = {},
     stylua = {}, -- Used to format Lua code
-
     ast_grep = {},
     astro = {},
-    codebook = {},
-    phpactor = {},
     phpantom_lsp = {},
+    bashls = {},
     quick_lint_js = {},
     stylua = {},
 
